0843e89937dfd42c35db9ef5f85227dfaaa0c75b
Ozge <ozgeakcaer@gmail.com>
2015-12-12 17:41:18 +0200

    veritabanindan template'e veriler aktarildi.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+def post_list(request):  
+    posts = Post.objects.filter(yaratilis_tarihi__lte=timezone.now()).order_by('yaratilis_tarihi')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
